adf02c13475c54fb53eedb108183e36306e7bd45
vippiv <470211273@qq.com>
2020-12-25 11:27:25 +0800

    动态表单

--- src/views/interaction/dynamicForm/index.js
+++ src/views/interaction/dynamicForm/index.js
@@ -1,15 +1,177 @@
 import React, { Component } from 'react'
-import { Form } from 'antd'
+import { Form, Input, Select, Col, Checkbox, Radio, DatePicker, Button } from 'antd'
 import './index.css'
 
 class DynamicForm extends Component {
-    render () {
-        return (
-            <>
-                动态表单
-            </>
-        )
-    }
+
+	dataSource = [
+		{
+			field: 'username',
+			fromType: 'input',
+			value: 'zwl'
+		}, {
+			field: 'nationality',
+			fromType: 'single-select',
+			value: '1'
+		}, {
+			field: 'hobby',
+			fromType: 'checkbox',
+			value: '1'
+		}, {
+			field: 'gender',
+			fromType: 'radio',
+			value: '1'
+		}, {
+			field: 'birthday',
+			fromType: 'datePicker',
+			value: '1'
+		}
+	]
+
+	renderInput = (obj) => {
+		const { getFieldDecorator } = this.props.form
+		return (
+			<>
+				<Form.Item label={obj.field || 'field'}>
+					{
+						getFieldDecorator(obj.field, {
+							rules: [{required: true, message:`Please choose ${obj.field}`}]
+						})(
+							<Input placeholder="请输入" />
+						)
+					}
+				</Form.Item>
+			</>
+		)
+	}
+
+	renderSelect = (obj) => {
+		const { getFieldDecorator } = this.props.form
+		return (
+			<>
+				<Form.Item label={obj.field || 'field'}>
+					{
+						getFieldDecorator(obj.field, {
+							rules: [{required: true, message: `Please choose ${obj.field}`}]
+						})(
+							<Select placeholder='请选择'>
+							</Select>
+						)
+					}
+					
+				</Form.Item>
+			</>
+		)
+	}
+
+	renderRadio = (obj) => {
+		const { getFieldDecorator } = this.props.form
+		return (
+			<>
+				<Form.Item label={obj.field || 'field'}>
+					{
+						getFieldDecorator(obj.field, {
+							rules: [{required: true, message: `Please choose ${obj.field}`}]
+						})(
+							<Radio.Group>
+								<Radio value={1}>male</Radio>
+								<Radio value={2}>female</Radio>
+								<Radio value={3}>unknown</Radio>
+							</Radio.Group>
+						)
+					}
+					
+				</Form.Item>
+			</>
+		)
+	}
+
+	renderCheckbox = (obj) => {
+		const { getFieldDecorator } = this.props.form
+		return (
+			<>
+				<Form.Item label={obj.field || 'field'}>
+					{
+						getFieldDecorator(obj.field, {
+							rules: [{ required: true, message: `Please choose ${obj.field}`}],
+							initialValue: ['3']
+						})(
+							<Checkbox.Group options={obj.plainOptions || ['apple', 'juice']} />
+						)
+					}
+				</Form.Item>
+			</>
+		)
+	}
+
+	renderDatePicker = (obj) => {
+		const { getFieldDecorator } = this.props.form
+		return (
+			<>
+				<Form.Item label={obj.field || 'field'}>
+					{
+						getFieldDecorator(obj.field, {
+							rules: [{required: true, message: 'Please choose date'}]
+						})(
+							<DatePicker />
+						)
+					}
+				</Form.Item>
+			</>
+		)
+	}
+
+	renderElement (obj) {
+		switch (obj.fromType) {
+			case 'input':
+				return this.renderInput(obj)
+			case 'single-select':
+				return this.renderSelect(obj)
+			case 'checkbox':
+				return this.renderCheckbox(obj)
+			case 'radio':
+				return this.renderRadio(obj)
+			case 'datePicker':
+				return this.renderDatePicker(obj)
+			default:
+				return ''
+		}
+	}
+
+	createForm = () => {
+		return this.dataSource.map((item, index) => {
+			let uniqueKey = Math.floor(Math.random() * 100000) + index
+			return (
+				<Col span={8} key={uniqueKey}>
+					{ this.renderElement(item) }
+				</Col>
+			)
+		})
+	}
+
+	handleSubmit = () => {
+		this.props.form.validateFields((err, formObj) => {
+			if (!err) {
+				console.log('submit')
+			}
+		})
+	}
+
+	render () {
+		return (
+			<div className="dynamic-from-container">
+				<h2>动态表单（修改数据即可生成对应表单项）</h2>
+				<Form layout="inline">
+					{this.createForm()}
+					<Col span={24} style={{textAlign: 'right'}}>
+						<Form.Item>
+							<Button type="primary" htmlType="submit" onClick={this.handleSubmit}>submit</Button>
+						</Form.Item>
+					</Col>
+				</Form>
+			</div>
+		)
+	}
 }
 
 export default Form.create()(DynamicForm)
